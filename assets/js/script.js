5e7e1f045cfdd6bb22c682538fa05eedfe192e00
Yaakov Ben Haim <yaakov@yaakovs-macbook-air.com>
2024-04-27 22:17:21 -0300

    feat: forms script and contact section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,57 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+/**
+ * script for the CTA
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector('.cta-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const emailInput = document.querySelector('input[name="email"]');
+    const userEmail = emailInput.value;
+
+    window.location.href = "mailto:contact@360brave.com?subject=Free Consultation&body=" + encodeURIComponent("sender e-mail: " + userEmail);
+  });
+});
+
+
+/**
+ * script for the Contact secrtion
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector('.contact-form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nameInput = document.getElementById('name').value;
+    const phoneInput = document.getElementById('phone').value;
+    const emailInput = document.getElementById('email').value;
+    const messageInput = document.getElementById('message').value;
+
+    // Montando o corpo do e-mail com os dados do formulário
+    const emailBody = "Name: " + nameInput + "\n" +
+                      "Phone: " + phoneInput + "\n" +
+                      "Email Address: " + emailInput + "\n" +
+                      "Message: " + messageInput;
+
+    // Montando o link 'mailto' com o endereço de e-mail de destino e o corpo do e-mail
+    const mailtoLink = "mailto:contact@360brave.com" +
+                       "?subject=Contact Form Submission" +
+                       "&body=" + encodeURIComponent(emailBody);
+
+    // Criando um elemento de link 'a' com o link 'mailto' como href
+    const link = document.createElement("a");
+    link.href = mailtoLink;
+
+    // Clicando no link programaticamente para abrir o cliente de e-mail do dispositivo
+    link.click();
+  });
+});
+
